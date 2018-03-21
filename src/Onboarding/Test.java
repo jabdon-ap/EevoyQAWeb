927e47eaed57e99f59c6dab80b402efe93d5ff7b
AppPartner <AppPartner@192.168.88.156>
2018-03-21 17:43:23 -0400

    first commit

---
+++ src/Onboarding/Test.java
@@ -0,0 +1,24 @@
+package Onboarding;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",  "/Users/AppPartner/Documents/Drivers/chromedriver");
+		String url = "http://eevoy-web-dev.s3-website-us-east-1.amazonaws.com/login";
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+		WebElement loginEmail = driver.findElement(By.name("email"));
+		WebElement loginPassword = driver.findElement(By.name("password"));
+		WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/main/form/button"));
+		
+		loginEmail.sendKeys("jabdon+101@apppartner.com");
+		loginPassword.sendKeys("SuperBoise1");
+		loginButton.click();
+	}
+
+}
